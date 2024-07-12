1cc484340b1c6b6778aaac73609bc4a00b5428e9
joao victor da silva cardoso <pealq63@gmail.com>
2024-07-12 17:16:57 -0300

    alterado o botao lua e sol

--- modoClaroEscuro.js
+++ modoClaroEscuro.js
@@ -1,15 +1,24 @@
 const modoClaro = document.querySelector('#modo__claro');
 const elemento = document.querySelector('body');
-const navBar = document.querySelector('nav')
+const navBar = document.querySelector('nav');
+const sol = document.querySelector('.bi-sun');
+const lua = document.querySelector('.bi-moon');
+
+console.log(sol)
 
 modoClaro.addEventListener('click', () =>{
     const modo = modoClaro.checked ? 'light' : 'dark'
     elemento.setAttribute("data-bs-theme", modo)
+
     if (navBar.classList.contains('bg-dark')) {
         navBar.classList.remove('bg-dark');
         navBar.classList.add('bg-light');
+        sol.classList.add('d-none');
+        lua.classList.remove('d-none')
     } else {
         navBar.classList.remove('bg-light');
         navBar.classList.add('bg-dark');
+        sol.classList.remove('d-none');
+        lua.classList.add('d-none')
     }
 })
