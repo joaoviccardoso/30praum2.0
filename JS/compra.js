9d38202e236fb904a3749a2dabc68a7785f1804f
joao victor da silva cardoso <pealq63@gmail.com>
2024-07-31 15:43:22 -0300

    foi criado a verificação de tamanho e quantidade

--- JS/compra.js
+++ JS/compra.js
@@ -1,14 +1,34 @@
 const btns = document.querySelectorAll('.btns');
 const imagem = document.querySelectorAll('.produtos');
 const modal = document.querySelector('.resuldadoDaCompra');
+const qtd = document.querySelectorAll('.qtd');
+const tamanho = document.querySelectorAll('.tamanho');
+const nomeModal = document.querySelector('#staticBackdropLabel');
 
-function btnClick(nomeDoProtudo){
-   modal.innerHTML = `<p>${nomeDoProtudo}, foi adicionado no carrinho de compras</p>`
+function btnClick(nomeDoProtudo, qtdEscolhida, tamanhoEscolhida){
+   nomeModal.innerHTML = `<h1>Adicionado no carrinho</h1>` 
+   modal.innerHTML = `<p>${nomeDoProtudo}, tamanho ${tamanhoEscolhida}, quantidade do item ${qtdEscolhida} foi adicionado no carrinho de compras</p>`
+}
+
+function cancelado(){
+    nomeModal.innerHTML = `<h1>Pedido cancelado</h1>`
+    modal.innerHTML = `<p>pedido foi cancelado, coloque um tamanho e uma quantidade</p>`
 }
 
 for (let i = 0; i < btns.length; i++) {
     const nomeDoProtudo = imagem[i].getAttribute('alt');
     btns[i].addEventListener('click', () =>{
-        btnClick(nomeDoProtudo);
+        const qtdEscolhida = qtd[i].value;
+        if (qtdEscolhida == "") {
+            cancelado();
+            return
+        }
+        const tamanhoEscolhida = tamanho[i].value;
+        if (tamanhoEscolhida == "Tamanho") {
+            cancelado();
+            return
+        }
+        btnClick(nomeDoProtudo, qtdEscolhida, tamanhoEscolhida);
     })
 } 
+
