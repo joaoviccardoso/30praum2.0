6879351d59c60c123a591da60eaa7f197a61eb47
joao victor da silva cardoso <pealq63@gmail.com>
2024-08-01 16:13:51 -0300

    colocado o container para pediddos no offcanvas

--- JS/compra.js
+++ JS/compra.js
@@ -3,20 +3,34 @@ const imagem = document.querySelectorAll('.produtos');
 const modal = document.querySelector('.resuldadoDaCompra');
 const qtd = document.querySelectorAll('.qtd');
 const tamanho = document.querySelectorAll('.tamanho');
-const nomeModal = document.querySelector('#staticBackdropLabel');
+const nomeModal = document.querySelector('#offcanvasRightLabel');
+const valorDosProdutos = document.querySelectorAll('.preco');
 
-function btnClick(nomeDoProtudo, qtdEscolhida, tamanhoEscolhida){
+
+function btnClick(nomeDoProtudo, qtdEscolhida, tamanhoEscolhida, caminhoDaImg, valorDoProduto){
    nomeModal.innerHTML = `<h1>Adicionado no carrinho</h1>` 
-   modal.innerHTML = `<p>${nomeDoProtudo}, tamanho ${tamanhoEscolhida}, quantidade do item ${qtdEscolhida} foi adicionado no carrinho de compras</p>`
+   modal.innerHTML = `
+    <div class="box d-flex container">
+        <img src="${caminhoDaImg}" width="100" height="100"/>
+        <div class='container d-flex flex-column  justify-content-center'>
+            <p class="texto-loja">${nomeDoProtudo}</p>
+            <p class="texto-loja">Tamanho ${tamanhoEscolhida}</p> 
+            <p class="texto-loja">Quantidade ${qtdEscolhida}</p>
+            ${valorDoProduto.outerText}
+        </div>
+    </div>        
+   `
 }
 
 function cancelado(){
-    nomeModal.innerHTML = `<h1>Pedido cancelado</h1>`
-    modal.innerHTML = `<p>pedido foi cancelado, coloque um tamanho e uma quantidade</p>`
+    nomeModal.innerHTML = `<h1 id="offcanvasRightLabel">Pedido cancelado</h1>`
+    modal.innerHTML = `<p class="texto-loja">pedido foi cancelado, coloque um tamanho e uma quantidade</p>`
 }
 
 for (let i = 0; i < btns.length; i++) {
     const nomeDoProtudo = imagem[i].getAttribute('alt');
+    const caminhoDaImg = imagem[i].getAttribute('src');
+    const valorDoProduto = valorDosProdutos[i];
     btns[i].addEventListener('click', () =>{
         const qtdEscolhida = qtd[i].value;
         if (qtdEscolhida == "") {
@@ -28,7 +42,7 @@ for (let i = 0; i < btns.length; i++) {
             cancelado();
             return
         }
-        btnClick(nomeDoProtudo, qtdEscolhida, tamanhoEscolhida);
+        btnClick(nomeDoProtudo, qtdEscolhida, tamanhoEscolhida, caminhoDaImg, valorDoProduto);
     })
 } 
 
