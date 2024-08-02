6f1e0e7cf7893e310c2f41e18d56a4c33f4a4c75
joao victor da silva cardoso <pealq63@gmail.com>
2024-08-02 16:27:00 -0300

    adicionado o localstorage no caarrinho de comprar

--- JS/compra.js
+++ JS/compra.js
@@ -1,48 +1,102 @@
-const btns = document.querySelectorAll('.btns');
-const imagem = document.querySelectorAll('.produtos');
-const modal = document.querySelector('.resuldadoDaCompra');
-const qtd = document.querySelectorAll('.qtd');
-const tamanho = document.querySelectorAll('.tamanho');
-const nomeModal = document.querySelector('#offcanvasRightLabel');
-const valorDosProdutos = document.querySelectorAll('.preco');
+const btns = document.querySelectorAll(".btns");
+const imagem = document.querySelectorAll(".produtos");
+const modal = document.querySelector(".resuldadoDaCompra");
+const qtd = document.querySelectorAll(".qtd");
+const tamanho = document.querySelectorAll(".tamanho");
+const nomeModal = document.querySelector("#offcanvasRightLabel");
+const valorDosProdutos = document.querySelectorAll(".preco");
 
+const carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
 
-function btnClick(nomeDoProtudo, qtdEscolhida, tamanhoEscolhida, caminhoDaImg, valorDoProduto){
-   nomeModal.innerHTML = `<h1>Adicionado no carrinho</h1>` 
-   modal.innerHTML += `
-    <div class="box d-flex container">
-        <img src="${caminhoDaImg}" width="100" height="100"/>
-        <div class='container d-flex flex-column  justify-content-center'>
-            <p class="texto-loja">${nomeDoProtudo}</p>
-            <p class="texto-loja">Tamanho ${tamanhoEscolhida}</p> 
-            <p class="texto-loja">Quantidade ${qtdEscolhida}</p>
-            ${valorDoProduto.outerText}
-        </div>
-    </div>        
-   `
+// function btnClick(pedidoDecompra) {
+//   nomeModal.innerHTML = `<h1>Adicionado no carrinho</h1>`;
+//   modal.innerHTML += `
+//     <div class="box d-flex container">
+//         <img src="${pedidoDecompra.img}" width="100" height="100"/>
+//         <div class='container d-flex flex-column  justify-content-center'>
+//             <p class="texto-loja">${pedidoDecompra.nome}</p>
+//             <p class="texto-loja">Tamanho ${pedidoDecompra.tamanha}</p> 
+//             <p class="texto-loja">Quantidade ${pedidoDecompra.qtd}</p>
+//             ${pedidoDecompra.qtd}
+//         </div>
+//     </div>        
+//    `;
+// }
+
+function criarElement(pedidoDecompra){
+    const div = document.createElement('div');
+    div.classList.add('box', 'd-flex', 'container')
+
+    const img = document.createElement('img');
+    img.setAttribute('src', pedidoDecompra.img);
+
+    const caixaInfomacao = document.createElement('div');
+    caixaInfomacao.classList.add('container', 'd-flex', 'flex-column',  'justify-content-center');
+
+    const nome = document.createElement('p'); // Corrigido: usando document.createElement
+    nome.classList.add('textoloja')
+    nome.textContent = pedidoDecompra.nome
+
+    const tamanho = document.createElement('p'); // Corrigido: usando document.createElement
+    tamanho.classList.add('textoloja')
+    tamanho.textContent = pedidoDecompra.tamanho
+
+    const quantidade = document.createElement('p'); // Corrigido: usando document.createElement
+    quantidade.classList.add('textoloja')
+    quantidade.textContent = pedidoDecompra.qtd
+
+    const valor = document.createElement('p'); // Corrigido: usando document.createElement
+    valor.classList.add('textoloja')
+    valor.textContent = pedidoDecompra.valor
+
+    caixaInfomacao.append(nome)
+    caixaInfomacao.append(tamanho)
+    caixaInfomacao.append(quantidade)
+    caixaInfomacao.append(valor)
+
+    div.append(caixaInfomacao)
+
+    return div
 }
 
-function cancelado(){
-    nomeModal.innerHTML = `<h1 id="offcanvasRightLabel">Pedido cancelado</h1>`
-    modal.innerHTML = `<p class="texto-loja">pedido foi cancelado, coloque um tamanho e uma quantidade</p>`
+function cancelado() {
+  nomeModal.innerHTML = `<h1 id="offcanvasRightLabel">Pedido cancelado</h1>`;
+  modal.innerHTML = `<p class="texto-loja">pedido foi cancelado, coloque um tamanho e uma quantidade</p>`;
 }
 
 for (let i = 0; i < btns.length; i++) {
-    const nomeDoProtudo = imagem[i].getAttribute('alt');
-    const caminhoDaImg = imagem[i].getAttribute('src');
-    const valorDoProduto = valorDosProdutos[i];
-    btns[i].addEventListener('click', () =>{
-        const qtdEscolhida = qtd[i].value;
-        if (qtdEscolhida == "") {
-            cancelado();
-            return
-        }
-        const tamanhoEscolhida = tamanho[i].value;
-        if (tamanhoEscolhida == "Tamanho") {
-            cancelado();
-            return
-        }
-        btnClick(nomeDoProtudo, qtdEscolhida, tamanhoEscolhida, caminhoDaImg, valorDoProduto);
-    })
-} 
+  const nomeDoProtudo = imagem[i].getAttribute("alt");
+  const caminhoDaImg = imagem[i].getAttribute("src");
+  const valorDoProduto = valorDosProdutos[i];
+  console.log(valorDoProduto);
+  btns[i].addEventListener("click", () => {
+    const qtdEscolhida = qtd[i].value;
+    if (qtdEscolhida == "") {
+      cancelado();
+      return;
+    }
+    const tamanhoEscolhida = tamanho[i].value;
+    if (tamanhoEscolhida == "Tamanho") {
+      cancelado();
+      return;
+    }
+
+    const pedidoDecompra = {
+      img: caminhoDaImg,
+      nome: nomeDoProtudo,
+      valor: valorDoProduto.outerText,
+      qtd: qtdEscolhida,
+      tamanho: tamanhoEscolhida,
+    };
+    carrinho.push(pedidoDecompra);
+    localStorage.setItem("carrinho", JSON.stringify(carrinho));
+    modal.append(criarElement(pedidoDecompra))
+  });
+}
+
+carrinho.forEach(pedidoDecompra => {
+    const tarefa = criarElement(pedidoDecompra);
+    modal.append(tarefa)
+});
+
 
