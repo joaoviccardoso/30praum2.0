61d03106d83d02225b1e1413a5bf691217804455
joao victor da silva cardoso <pealq63@gmail.com>
2024-08-07 01:17:27 -0300

    adicionado a funçao de remover todos os itens do carrinho de compra

--- JS/compra.js
+++ JS/compra.js
@@ -5,58 +5,30 @@ const qtd = document.querySelectorAll(".qtd");
 const tamanho = document.querySelectorAll(".tamanho");
 const nomeModal = document.querySelector("#offcanvasRightLabel");
 const valorDosProdutos = document.querySelectorAll(".preco");
+const limpar = document.querySelector('.limparTudo')
 
 const carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
 
-function criarElement(pedidoDecompra){
-    const div = document.createElement('div');
-    div.classList.add('box', 'd-flex', 'container', 'm-2')
+function btnClick(pedidoDecompra) {
+  modal.innerHTML += `
+    <div class="box d-flex container m-2 tarefas">
+        <img src="./${pedidoDecompra.img}" width="100" height="100"/>
+        <div class='container d-flex flex-column  justify-content-center'>
+            <p class="texto-loja">${pedidoDecompra.nome}</p>
+            <p class="texto-loja">Tamanho ${pedidoDecompra.tamanha}</p> 
+            <p class="texto-loja">Quantidade ${pedidoDecompra.qtd}</p>
+            ${pedidoDecompra.valor}
+        </div>
+    </div>        
+   `;
+}
 
-    const img = document.createElement('img');
-    img.setAttribute('src', pedidoDecompra.img);
-    img.setAttribute('width', '100');
-    img.setAttribute('height', '100')
-
-    const caixaInfomacao = document.createElement('div');
-    caixaInfomacao.classList.add('container', 'd-flex', 'flex-column',  'justify-content-center');
-
-    const nome = document.createElement('p'); // Corrigido: usando document.createElement
-    nome.classList.add('textoloja')
-    nome.textContent = `Produto: ${pedidoDecompra.nome}`
-
-    const tamanho = document.createElement('p'); // Corrigido: usando document.createElement
-    tamanho.classList.add('textoloja')
-    tamanho.textContent = `Tamanho: ${pedidoDecompra.tamanho}`
-
-    const quantidade = document.createElement('p'); // Corrigido: usando document.createElement
-    quantidade.classList.add('textoloja')
-    quantidade.textContent = `Quantidade: ${pedidoDecompra.qtd}`
-
-    const valor = document.createElement('p'); // Corrigido: usando document.createElement
-    valor.classList.add('textoloja')
-    valor.textContent = pedidoDecompra.valor
-
-    const divLixeira = document.createElement('span');
-    divLixeira.classList.add('d-flex', 'justify-content-end');
-
-    const btnLixeira = document.createElement('button');
-    btnLixeira.classList.add('.btn__lixeira')
-    const imgBotao = document.createElement('img');
-    imgBotao.setAttribute('src', '../img/trash.svg');
-
-    btnLixeira.append(imgBotao)
-    divLixeira.append(btnLixeira)
-
-    caixaInfomacao.append(nome)
-    caixaInfomacao.append(tamanho)
-    caixaInfomacao.append(quantidade)
-    caixaInfomacao.append(valor)
-    caixaInfomacao.append(btnLixeira)
-
-    div.append(img)
-    div.append(caixaInfomacao)
-
-    return div
+limpar.onclick = () =>{
+  const seletor = modal.querySelectorAll(".tarefas");
+  for (let i = 0; i < seletor.length; i++) {
+    seletor[i].remove()
+    localStorage.clear()
+  }
 }
 
 function cancelado() {
@@ -68,7 +40,7 @@ for (let i = 0; i < btns.length; i++) {
   const caminhoDaImg = imagem[i].getAttribute("src");
   const valorDoProduto = valorDosProdutos[i];
   console.log(valorDoProduto);
-  btns[i].addEventListener("click", () => {
+  btns[i].onclick = function criarElemento() {
     const qtdEscolhida = qtd[i].value;
     if (qtdEscolhida == "") {
       cancelado();
@@ -89,16 +61,12 @@ for (let i = 0; i < btns.length; i++) {
     };
     carrinho.push(pedidoDecompra);
     localStorage.setItem("carrinho", JSON.stringify(carrinho));
-    modal.append(criarElement(pedidoDecompra))
-  });
+    btnClick(pedidoDecompra)
+  };
 }
 
-carrinho.forEach(pedidoDecompra => {
-    const tarefa = criarElement(pedidoDecompra);
-    modal.append(tarefa)
+document.addEventListener('DOMContentLoaded', function() {
+  for (let i = 0; i < carrinho.length; i++) {
+    btnClick(carrinho[i]);
+  }
 });
-
-btnLixeira.addEventListener('onclick', () =>{
-    modal.removeChild(div)
-})
-
