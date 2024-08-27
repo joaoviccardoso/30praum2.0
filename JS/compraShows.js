b011a18182616fe14c618707d30e2df59ab8672e
joao victor da silva cardoso <pealq63@gmail.com>
2024-08-27 10:19:25 -0300

    foi pego o id do botao que vai ser  separado do numero que todos eles tem com isso podemos ter acesso a lista dos outros elementos, com isso foi construido a lista de carrinhoShows e foi criado os objetos para adicionar na lista

--- JS/compraShows.js
+++ JS/compraShows.js
@@ -3,9 +3,22 @@ const cidadesDosShows = document.querySelectorAll('.cidade__show');
 const precosDosIngressos = document.querySelectorAll('.preco__shows');
 const cantoresDosShows = document.querySelectorAll('.cantores');
 
+const carrinhoShows = [];
+
 btnsDeCompraDosIngressos.forEach(btn => btn.addEventListener('click', btnClick))
 
 function btnClick(){
     const elementoDoBtnClick = document.getElementById(this.id)
-    console.log(elementoDoBtnClick)
+    const idDoBtn = elementoDoBtnClick.getAttribute('id');
+    const numeroDoElemento = idDoBtn.split('-')[1]
+
+    const ingresso = {
+        cidade: cidadesDosShows[numeroDoElemento].textContent,
+        valor: precosDosIngressos[numeroDoElemento].textContent,
+        cantores: cantoresDosShows[numeroDoElemento].textContent
+    }
+
+    carrinhoShows.push(ingresso);
+    
+    console.log(carrinhoShows)
 }
